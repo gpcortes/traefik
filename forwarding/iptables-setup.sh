832d60df39c5fe905f300f62025c9c854aa5486a
Gustavo Côrtes <gpcortes@gmail.com>
2024-06-04 20:27:12 -0300

    Teste all host

--- forwarding/iptables-setup.sh
+++ forwarding/iptables-setup.sh
@@ -4,7 +4,7 @@
 DOCKER_INTERNAL_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 
 # Adicione a regra de pré-rota para redirecionar a porta 22
-iptables -t nat -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination $DOCKER_INTERNAL_IP:22
+iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $DOCKER_INTERNAL_IP:22
 
 # Adicione a regra para garantir que a resposta retorne pelo caminho correto
 iptables -t nat -A POSTROUTING -p tcp -d $DOCKER_INTERNAL_IP --dport 22 -j MASQUERADE
