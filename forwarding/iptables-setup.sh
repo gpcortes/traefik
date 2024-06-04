5b0d19f8451f09623bdee8a795004621b969d185
Gustavo Côrtes <gpcortes@gmail.com>
2024-06-04 20:17:54 -0300

    Change redirect tcp port

--- forwarding/iptables-setup.sh
+++ forwarding/iptables-setup.sh
@@ -4,12 +4,10 @@
 DOCKER_INTERNAL_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 
 # Adicione a regra de pré-rota para redirecionar a porta 22
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $DOCKER_INTERNAL_IP:22
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $DOCKER_INTERNAL_IP:80
+iptables -t nat -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination $DOCKER_INTERNAL_IP:22
 
 # Adicione a regra para garantir que a resposta retorne pelo caminho correto
 iptables -t nat -A POSTROUTING -p tcp -d $DOCKER_INTERNAL_IP --dport 22 -j MASQUERADE
-iptables -t nat -A POSTROUTING -p tcp -d $DOCKER_INTERNAL_IP --dport 80 -j MASQUERADE
 
 # Manter o container em execução (evitar que saia imediatamente)
 tail -f /dev/null
