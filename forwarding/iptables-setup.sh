f75ef2a00f05907acce0fc374f6341dbb8e959fb
Gustavo Côrtes <gpcortes@gmail.com>
2024-11-16 01:37:02 -0300

    Final configuration

--- forwarding/iptables-setup.sh
+++ forwarding/iptables-setup.sh
@@ -3,11 +3,28 @@
 # Defina a variável para o IP de destino (substitua pelo IP correto)
 DOCKER_INTERNAL_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 
-# Adicione a regra de pré-rota para redirecionar a porta 22
-iptables -t nat -A PREROUTING -p tcp --dport 2222 -j DNAT --to-destination $DOCKER_INTERNAL_IP:2222
+# Define porta de acesso ssh de entrada do container
+INCOMING_SSH_PORT=${SSH_PORT:-2222}
 
-# Adicione a regra para garantir que a resposta retorne pelo caminho correto
-iptables -t nat -A POSTROUTING -p tcp -d $DOCKER_INTERNAL_IP --dport 2222 -j MASQUERADE
+# Define porta de acesso ssh do host docker
+OUTGOING_SSH_PORT=${SSH_HOST_PORT:-22}
+
+# Limpar regras existentes (opcional, cuidado ao usar em produção)
+iptables -F
+iptables -t nat -F
+
+# Redirecionar conexões que chegam na porta 2222 para host.docker.internal:22
+iptables -t nat -A PREROUTING -p tcp --dport $INCOMING_SSH_PORT -j DNAT --to-destination $DOCKER_INTERNAL_IP:$OUTGOING_SSH_PORT
+
+# Alterar o endereço de origem dos pacotes para o IP da interface de saída
+# (Para garantir que a resposta volte pelo caminho correto)
+iptables -t nat -A POSTROUTING -p tcp --dport $OUTGOING_SSH_PORT -d $DOCKER_INTERNAL_IP -j MASQUERADE
+
+# Permitir o tráfego de retorno da conexão estabelecida
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Permitir o tráfego para host.docker.internal:22
+iptables -A FORWARD -p tcp -d $DOCKER_INTERNAL_IP --dport $OUTGOING_SSH_PORT -j ACCEPT
 
 # Manter o container em execução (evitar que saia imediatamente)
 tail -f /dev/null
